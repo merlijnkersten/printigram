8be690de2cc6482029dc40aafd6f45db8df6948d
merlijnkersten <kerstenmerlijn@gmail.com>
2019-12-15 23:33:05 +0000

    Printigram version 2.0
    New in this version
    - Text is smaller. This means there is no longer a limit on the length of words, since the longest word in the English language (Honorificabilitudinitatibus) can be printed in full on Printi Minis,
    
    - You can now switch between Printi's without quiting the programme, by typing 'C' or 'CHANGE',

--- Printigram.py
+++ Printigram.py
@@ -1,5 +1,6 @@
 # PRINTIGRAM 
-# 16 July 2019
+# First version: 16 July 2019
+# Second version: 15 December 2019
 # Tested for Python 3.7.2 on Windows 10 Pro
 # The programme only works on Windows; need to change font to work on other OS.
 
@@ -8,64 +9,50 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 import printipigeon as pp
 
-# The Printigram function
+# The following two functions set the maximum word length and the width in pixels of the printi in question, either Fons's (printi.me/) or any other.
+
+def maxlength(printi_name):
+    if printi_name == '': 
+        return 41
+    else:                   
+        return 27
+def width(printi_name):
+    if printi_name == '': 
+        return 576
+    else:                   # All Printi Mini's
+        return 384
+
+# The Printigram function. Turns an input message into a text image which is sent to Printi.
 def printigram(inputtext):                                                      
     lines = []                                                                  # Create empty list to store individual lines
     while inputtext != '':                                                      # Continue creating lines until the string is empty (i.e. the whole string has been added to lines)
-        if len(inputtext) <= maxlength:                                         # If string is not longer than the maximum line length, add it to the lines list.
+        if len(inputtext) <= maxlength(printi_name):                            # If string is not longer than the maximum line length, add it to the lines list.
             lines.append(inputtext)
             break
         else:                                                                   # If it is, add the longest possible string of words to the lines list (i.e. until the last space before the break)
-            linebreak = inputtext.rfind(' ', 0, maxlength)
+            linebreak = inputtext.rfind(' ', 0, maxlength(printi_name))
             lines.append(inputtext[:linebreak])
             inputtext = inputtext[linebreak+1:]                                 # Delete the string of words you added to the lines list from the original string.
-    img = Image.new('RGB', (width,len(lines)*50-10),  color = 'white')          # Create an white image with the width of the printer and the length of the lines
-    fnt = ImageFont.truetype('C:/Windows/Fonts/Consola.ttf', size=40)           # Use the Consolas font. Only works on Windows.
+    img = Image.new('RGB', (width(printi_name),len(lines)*28-3),  color = 'white')          # Create an white image with the width of the printer and the length of the lines
+    fnt = ImageFont.truetype('C:/Windows/Fonts/Consola.ttf', size=25)           # Use the Consolas font. Only works on Windows.
     for i in range(len(lines)):
-        ImageDraw.Draw(img).text((0,i*50), lines[i], font=fnt, fill=(0,0,0))    # Write lines in black text.
+        ImageDraw.Draw(img).text((0,i*28), lines[i], font=fnt, fill=(0,0,0))    # Write lines in black text.
     img.save('text.png')
-    pp.send_from_path('text.png', printiname)                                      # Use Printi Pigeon to sent pictures to correct printer                                                                        # Quit driver
+    pp.send_from_path('text.png', printi_name)                                  # Use Printi Pigeon to sent pictures to correct printer                                                                        # Quit driver
     os.remove('text.png')                                                       # Remove image
 
-# Script to determine whether one of the words is too long to print. 
-def findlengths(inputtext):
-    words = inputtext.split(' ')
-    maxinputlength = 0
-    for i in range(len(words)):
-        if len(words[i]) > maxinputlength:
-            maxinputlength = len(words[i])
-    return maxinputlength
+# User-interactions. Ask which printi to sent messages to.
+print('\n PRINTIGRAM \n Type "QUIT" to exit \n Type "CHANGE" to switch to a different Printi ')
 
-# User-interactions.
-print('\n PRINTIGRAM')
-print(' Type "QUIT" to exit \n')
+printi_name = input('\n Which Printi? \n printi.me/')
 
-# Ask whether to send messages to printi.me or printi.me/mango (different address and different widths)
-printi = input(' Which printi? Type F (Fons), L (/luka) or M (/mango): ')
-while printi not in ['F', 'f', 'L', 'l', 'M', 'm']:   # Make sure that a Printi is chosen
-    printi = input('\n Whoops! Try again. Type F (Fons), L (/luka) or M (/mango): ')
-if printi == 'F' or printi == 'f':
-    printiname = 'printi'
-    width = 576                             # Width in pixels
-    maxlength = 25                          # Maximum number of characters that fit on one line
-elif printi == 'L' or printi == 'l':
-    printiname = 'luka'
-    width = 384
-    maxlength = 16
-else:
-    printiname = 'mango'
-    width = 384                             # Width in pixels
-    maxlength = 16                          # Maximum number of characters that fit on one line
-# If new Printi comes online: simply add its details and key above.
-
-# Ask for message input
 message = input('\n Message: ')
-while findlengths(message) > maxlength:     # Checks whether input words are short enough to print
-    print('One of your words is too long. They cannot be longer than ' + str(maxlength) + ' characters.\n')
-    message = input(' Message: ')
-while message != 'QUIT':                    # Regular text: send it to chosen printi
-    printigram(message)
+while message != 'QUIT' and message != 'Q':
+    if message == 'CHANGE' or message == 'C':                   # To change recipient Printi.
+        printi_name = input('\n Which Printi? \n printi.me/')
+    else:
+        printigram(message)
     message = input('\n Message: ')
-if message == 'QUIT':                       # 'QUIT': Quit programme
+if message == 'QUIT' or message == 'Q':             
     print()
     exit()
